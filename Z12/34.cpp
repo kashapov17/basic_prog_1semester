18fafee29755353f4683c2a3019927b782f9556f
Yaroslav <55892644+kashapovd@users.noreply.github.com>
2019-11-24 15:30:50 +0700

    new version

--- Z12/34.cpp
+++ Z12/34.cpp
@@ -2,16 +2,15 @@
 #include <fstream>
 using namespace std;
 
+#define FILE_PATH "/home/yaroslav/cpp_project/basic_prog/FILES/"
 #define FILE_NAME "z12_1.txt"
 
 int **make2d(size_t nrows, size_t ncols) {
     srand(time(NULL));
     int **arr;
     arr = new int*[nrows];
-    for(size_t k=0; k<nrows; k++) {
-        arr[k] = new int[ncols];
-    }
     for(size_t i=0; i<nrows; i++) {
+        arr[i] = new int[ncols];
         for(size_t j=0; j<ncols; j++) {
             arr[i][j] = rand()%10;
         }
@@ -19,22 +18,22 @@ int **make2d(size_t nrows, size_t ncols) {
     return arr;
 }
 
-void print2d(const int *const *arr, size_t nrows, size_t ncols, ofstream &ost) {
+void print2d(const int *const *arr, size_t nrows, size_t ncols, ofstream &ost, string file) {
     for(size_t i=0; i<nrows; i++) {
         for(size_t j=0; j<ncols; j++) {
             ost << arr[i][j] << " ";
         }
         ost << "\n";
     }
-    cout << "\nЗапись в файл " << FILE_NAME << " завершена\n\n";
+    cout << "\nЗапись в файл " << file << " завершена\n\n";
 }
 
 int main() {
     size_t ncols, nrows;
     int **arr;
     ofstream ost;
-
-    ost.open(FILE_NAME);
+    string file = string(FILE_PATH) + string(FILE_NAME);
+    ost.open(file);
     if(!ost.is_open()) {
         cerr << "Ошибка открытия файла\n";
         exit(0);
@@ -46,7 +45,7 @@ int main() {
     cin >> ncols;
 
     arr = make2d(nrows, ncols);
-    print2d(arr, nrows, ncols, ost);
+    print2d(arr, nrows, ncols, ost, file);
     ost.close();
     return 0;
 }
