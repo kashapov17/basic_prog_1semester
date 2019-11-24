2b7008c5ff24639788e94b41b6d295f48b2b19b7
Yaroslav <55892644+kashapovd@users.noreply.github.com>
2019-11-24 15:31:28 +0700

    new version

--- Z12/35.cpp
+++ Z12/35.cpp
@@ -2,15 +2,14 @@
 #include <fstream>
 using namespace std;
 
+#define FILE_PATH "/home/yaroslav/cpp_project/basic_prog/FILES/"
 #define FILE_NAME "z12_2.txt"
 
 int **make2d(size_t nrows, size_t ncols) {
     int **arr;
     arr = new int*[nrows];
-    for(size_t k=0; k<nrows; k++) {
-        arr[k] = new int[ncols];
-    }
     for(size_t i=0; i<nrows; i++) {
+        arr[i] = new int[ncols];
         for(size_t j=0; j<ncols; j++) {
             cout << "элемент " << i+1 << "," << j+1 << " = ";
             cin >> arr[i][j];
@@ -28,22 +27,23 @@ void fliplr(int **arr, size_t nrows, size_t ncols) {
     }
 }
 
-void print_fliplr(const int *const *arr, size_t nrows, size_t ncols, ostream &ost) {
+void print_fliplr(const int *const *arr, size_t nrows, size_t ncols, ostream &ost, string file) {
     for(size_t i=0; i<nrows; i++) {
         for(size_t j=0; j<ncols; j++) {
             ost << arr[i][j] << " ";
         }
         ost << "\n";
     }
-    cout << "\nЗапись в файл " << FILE_NAME << " завершена\n\n";
+    cout << "\nЗапись в файл " << file << " завершена\n\n";
 }
 
 int main() {
     int **arr;
     size_t nrows, ncols;
     ofstream ost;
+    string file = string(FILE_PATH) + string(FILE_NAME);
 
-    ost.open(FILE_NAME);
+    ost.open(file);
     if(!ost.is_open()) {
         cerr << "Ошибка чтения файла\n";
         exit(0);
@@ -56,7 +56,7 @@ int main() {
 
     arr = make2d(nrows, ncols);
     fliplr(arr, nrows, ncols);
-    print_fliplr(arr, nrows, ncols, ost);
+    print_fliplr(arr, nrows, ncols, ost, file);
     ost.close();
     return 0;
 }
